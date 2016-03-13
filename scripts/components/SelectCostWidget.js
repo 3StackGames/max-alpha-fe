e384deee862703762d556bc8c35431159d1bb8e2
arjun <sarodearjun57@gmail.com>
2016-03-13 12:09:35 -0700

    Simplify logic for cost UI

--- scripts/components/SelectCostWidget.js
+++ scripts/components/SelectCostWidget.js
@@ -4,18 +4,6 @@ import { colors } from '../utils'
 import { Orb } from '.'
 
 export default class SelectCostWidget extends Component {
-  constructor(props) {
-    super(props)
-
-    this.remainingColorValues = this.calculateRemaining(props.cost, props.paidCost)
-    console.log(this.remainingColorValues)
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.remainingColorValues = this.calculateRemaining(nextProps.cost.colors, nextProps.paidCost.colors)
-    console.log(this.remainingColorValues)
-  }
-
   render() {
     return <div className='SelectCostWidget u-flex u-flex-center'>
       {
@@ -34,39 +22,8 @@ export default class SelectCostWidget extends Component {
     </div>
   }
 
-  calculateRemaining(cost, paidColors) {
-    const paidColorsCopy = {...paidColors}
-    return Object.keys(paidColorsCopy)
-      .reduce((prev, curr) => {
-        if (paidColorsCopy[curr] > 0) {
-          paidColorsCopy[curr]--
-          if (prev[curr] > 0) {
-            prev[curr]--
-          } else if (prev[colors.COLORLESS] > 0) {
-            prev[colors.COLORLESS]--
-          }
-        }
-        return prev
-      }, {...cost})
-  }
-
-  isAccepting(color) {
-    // console.log('PAID COPY: ', paidCostCopy.colors)
-    // console.log('PAID: ', this.props.paidCost.colors)
-    // console.log('COST: ', this.props.cost.colors)
-    // console.log('REMAINING: ', remainingColorValues)
-
-    return this.remainingColorValues[color] > 0
-      || this.remainingColorValues[colors.COLORLESS] > 0
-  }
-
   canPay(color) {
-    console.log('vvvvvvv vvvvvvv')
-    console.log(color)
-    console.log('accepting? ', this.isAccepting(color))
-    console.log('have? ', this.props.paidCost.colors[color] < this.props.colorResources[color])
-    console.log('^^^^^^^ ^^^^^^^')
-    return this.isAccepting(color) && this.props.paidCost.colors[color] < this.props.colorResources[color]
+    return this.props.paidCost.colors[color] < this.props.colorResources[color]
   }
 
   orbStatus(color) {
@@ -82,71 +39,3 @@ export default class SelectCostWidget extends Component {
     this.props.assignCost(color, this.props.paidCost.colors[color] + modifier)
   }
 }
-
-// const SelectCostWidget = ({ cost, paidCost, colorResources, assignCost }) => {
-//   const paidCostCopy = {...paidCost}
-//   const remainingColorValues = Object.keys(paidCostCopy.colors)
-//     .reduce((prev, curr) => {
-//       if (paidCostCopy.colors[curr] > 0) {
-//         paidCostCopy.colors[curr]--
-//         if (prev[curr] > 0) {
-//           prev[curr]--
-//         } else if (prev[colors.COLORLESS] > 0) {
-//           prev[colors.COLORLESS]--
-//         }
-//       }
-//       return prev
-//     }, {...cost.colors})
-
-//   const isAccepting = color => {
-//     return remainingColorValues[color] > 0
-//       || remainingColorValues[colors.COLORLESS] > 0
-//   }
-
-//   const canPay = color => {
-//     console.log('vvvvvvv vvvvvvv')
-//     console.log(color)
-//     console.log(isAccepting(color))
-//     console.log(paidCost.colors[color] < colorResources[color])
-//     console.log('^^^^^^^ ^^^^^^^')
-//     return isAccepting(color) && paidCost.colors[color] < colorResources[color]
-//   }
-
-//   const orbStatus = color => {
-//     if (!canPay(color)) return 'quiet'
-//     return 'normal'
-//   }
-
-//   const handleClick = (e, color) => {
-//     // if (!canPay(color)) return
-//     // const modifier = e.shiftKey ? -1 : 1
-//     console.log(paidCost.colors[color] + 1)
-//     assignCost(color, paidCost.colors[color] + 1)
-//   }
-
-//   // console.log('PAID: ', paidCost.colors)
-//   // console.log('COST: ', cost.colors)
-//   // console.log('REMAINING: ', remainingColorValues)
-//   return <div className='SelectCostWidget u-flex u-flex-center'>
-//     <button onClick={e => handleClick(e, 'COLORLESS')}>
-//       {paidCost.colors['COLORLESS']}
-//     </button>
-//   </div>
-// }
-
-// export default SelectCostWidget
-
-
-// {
-//   Object.keys(colorResources)
-//     .filter(color => colorResources[color] > 0)
-//     .map((color, i) => {
-//       return <Orb
-//         key={i}
-//         value={paidCost.colors[color]}
-//         color={color}
-//         type='costSelector'
-//         onClick={handleClick(color)} />
-//     })
-// }
-
